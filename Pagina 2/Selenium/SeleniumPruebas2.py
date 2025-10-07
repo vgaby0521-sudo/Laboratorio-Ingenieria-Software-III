a1a66f1cb152ac8a8cdfc23818b407df6125df08
vgaby0521-sudo <vgaby0521@gmail.com>
2025-10-07 11:30:59 -0500

    I.S.

--- Pagina 2/Selenium/SeleniumPruebas2.py
+++ Pagina 2/Selenium/SeleniumPruebas2.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 
-desktop_path = r"C:\Users\Hary Tatiana\Desktop"
+desktop_path = r"C:\Users\vgaby\Desktop"
 
 class TestCalculadora(unittest.TestCase):
 
